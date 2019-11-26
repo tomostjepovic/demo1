b1ace3ef2f973c13702882308e6945413748c167
Tomo Stjepović <tomo.stjepovic@omega-software.hr>
2019-11-26 14:00:10 +0100

    Dodavanje Datoteke Student.sql

---
+++ Student.sql
@@ -0,0 +1,6 @@
+SELECT
+ PunoIme = Ime + ' ' + Prezime, Adresa = a.Broj + ' ' + a.Ulica,
+ NazivFakulteta = f.NazivFakulteta
+FROM Omega.Student s
+ JOIN Omega.Adresa a ON a.ID = s.AdresaID
+ JOIN Omega.Fakultet f ON f.ID = s.ID
